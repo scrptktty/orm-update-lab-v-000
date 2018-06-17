bd679205a2279bfd8be70e088a0a8e63ef2d56b6
Kachel Wittig <kjwittig@gmail.com>
2018-06-17 22:33:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,9 +55,9 @@ class Student
 
   def self.new_from_db(row)
     new_student = self.new
-    new_student.id = new_student[0]
-    new_student.name = new_student[1]
-    new_student.grade = new_student[2]
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
     new_student
   end
 
