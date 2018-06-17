17188edbf5762d9846d0192bb36b3fb38734d40c
Kachel Wittig <kjwittig@gmail.com>
2018-06-17 22:44:46 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,10 +54,11 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = self.new(...) # FIXME
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
+
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    new_student = self.new(id, name, grade)
     new_student
   end
 
