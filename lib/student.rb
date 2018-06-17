b59d749f8f8850f2ae2a29a81cf9a770a47140aa
Kachel Wittig <kjwittig@gmail.com>
2018-06-17 22:48:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,4 +61,15 @@ class Student
     new_student
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name)
+  end
+
 end
